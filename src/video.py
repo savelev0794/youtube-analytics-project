7af0f2f3df875026a4207fb051a46924f8167eee
Egor Savelyev <savelev0794@mail.ru>
2023-10-09 21:29:45 +0500

    modify video.py

--- src/video.py
+++ src/video.py
@@ -9,17 +9,24 @@ class Video:
 
     def __init__(self, video_id) -> None:
         """Инициализация класса Video"""
-        self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                         id=video_id).execute()
-        self.video_id: str = video_id
-        self.video_title: str = self.video_response['items'][0]['snippet']['title']
-        self.url: str = f'https://www.youtube.com/watch?v={self.video_id}'
-        self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
-        self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
+        try:
+            self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                             id=video_id).execute()
+            self.video_id: str = video_id
+            self.title: str = self.video_response['items'][0]['snippet']['title']
+            self.url: str = f'https://www.youtube.com/watch?v={self.video_id}'
+            self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
+            self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
+        except IndexError:
+            self.video_id = video_id
+            self.title = None
+            self.url = None
+            self.like_count = None
+            self.view_count = None
 
     def __str__(self):
         """Возвращает название видео"""
-        return f"{self.video_title}"
+        return f"{self.title}"
 
     @classmethod
     def get_service(cls):
@@ -34,12 +41,12 @@ class PLVideo(Video):
         self.playlist_id = playlist_id
         self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
                                                          id=self.video_id).execute()
-        self.video_title = self.video_response['items'][0]['snippet']['title']
+        self.title = self.video_response['items'][0]['snippet']['title']
         self.url = f'https://www.youtube.com/watch?v={self.video_id}'
         self.view_count = self.video_response['items'][0]['statistics']['viewCount']
         self.like_count = self.video_response['items'][0]['statistics']['likeCount']
 
     def __str__(self):
         """Возвращает название видео"""
-        return f"{self.video_title}"
+        return f"{self.title}"
 
